f43701232903f17d4eecc5a63208fd2fdc6f864f
aofayemi <42700790+aofayemi@users.noreply.github.com>
2018-11-12 22:22:48 -0600

    add event listener and domcontent to improve mp3 player

--- pp-04/site.js
+++ pp-04/site.js
@@ -1,4 +1,28 @@
+'use strict'
 function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
-}
+
+  document.getElementByID("doorbell")innerHTML = "<button id='newPlayButton'>Play doorbell</button>";
+
+//};
+
+//Return if no javascript
+(function(){
+  if(typeof document.querySelector === 'undefined'){
+    return;
+  }
+});
+
+document.addEventListener('DOMContent', function(){
+console.log("DOM has loaded");
+doorbell.addEventListener('click', function(){
+  console.log('click');
+  ringDoorbell();  });
+  });
+
+//part three
+//event listenener
+button.addEventListener("click", function() {
+   ringDoorbell();
+});
